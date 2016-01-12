10944dbc61f292190c91e184ae5eb1efdd7f407c
caiyinxiang <caiyinxiang@letv.com>
2016-01-12 14:32:29 +0800

    refine

--- RemoveLinkedListElements.java
+++ RemoveLinkedListElements.java
@@ -1,9 +1,8 @@
-# http://www.lintcode.com/en/problem/remove-linked-list-elements/
+// http://www.lintcode.com/en/problem/remove-linked-list-elements/
 
-# Remove all elements from a linked list of integers that have value val.
-#
-# Example
-# Given 1->2->3->3->4->5->3, val = 3, you should return the list as 1->2->4->5
+// Remove all elements from a linked list of integers that have value val.
+// Example
+// Given 1->2->3->3->4->5->3, val = 3, you should return the list as 1->2->4->5
 
 /**
  * Definition for singly-linked list.
